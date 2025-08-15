9aea74b07c3eb1effa33415c1789c5b9566d8061
alfin-cs <alfinsathesh@gmail.com>
2025-08-15 23:35:20 +0300

    solve the double operation problem

--- changes.js
+++ changes.js
@@ -1,4 +1,20 @@
 let text = document.querySelector("p");
-let button = document.querySelectorAll("button");
-
-button;
+let main = document.querySelector(".operations");
+let a = [];
+let number = "";
+main.addEventListener("click", (event) => {
+  button = event.target;
+  if (button.tagName == "BUTTON") {
+    type(button.value);
+    console.log(a);
+  }
+});
+function type(value) {
+  if ("1234567890".includes(value)) {
+    console.log(number);
+    number = number.concat(value);
+  } else {
+    a.push(number);
+    a.push(value);
+  }
+}
